aaf64e9752474e0d51a3e803ff01dd01de833bbe
Samir <samirtula@mail.ru>
2022-06-28 00:49:47 +0300

    styles in react

--- React/react-theory/src/App.js
+++ React/react-theory/src/App.js
@@ -1,13 +1,9 @@
-import './App.css';
+import './App.scss';
 import React, {Component} from "react";
 import Car from "./Car/Car";
 
 class App extends Component {
-
-    /*
-    состояние элемента
-    */
-
+    //Состояние элемента
     state = {
         cars: [
             {name: 'Ford', year: 2018},
@@ -19,11 +15,6 @@ class App extends Component {
     }
 
     changeTitleHandler = newTitle => {
-        // console.log('React click')
-
-        // const oldTitle = this.state.pageTitle;
-        // const newTitle = oldTitle + ' (changed)';
-
         //передаем новое состояните. Передаем только то свойство которое хотим изменить
         this.setState({
             pageTitle: newTitle,
@@ -51,7 +42,7 @@ class App extends Component {
         cars[index] = car;
 
         this.setState({
-            cars:cars
+            cars: cars
             //cars можно и такую запись в случае если ключ и значение совпадают
         })
     }
@@ -63,7 +54,6 @@ class App extends Component {
         this.setState({
             cars
         })
-
     }
 
     //при каждом изменении состояния вызывается метод рендер
@@ -73,68 +63,46 @@ class App extends Component {
         const divStyle = {
             textAlign: 'center',
         };
-        /*
-        вешаем обработчик события
-        https://ru.reactjs.org/docs/events.html
-        */
+
+        // вешаем обработчик события
+        // https://ru.reactjs.org/docs/events.html
+
+        let cars = null;
+        if (this.state.showCars) {
+            cars = this.state.cars.map((car, index) => {
+                return (
+                    <Car
+                        //для оптимизации передается key
+
+                        //props name year onChangeTitle onChangeName onDelete
+                        key={index}
+                        name={car.name}
+                        year={car.year}
+                        onChangeTitle={this.changeTitleHandler.bind(this, car.name)}
+
+                        //1. функция не создает своего контекста(стрелочные функции)
+                        onChangeName={event => this.onChangeName(event.target.value, index)}
+                        //2. функция имеет свой контекст, мы его связали bindom с контекстом вскго класса App*/
+                        onDelete={this.deleteHandler.bind(this, index)}
+                    />
+                )
+            })
+        }
+
         return (
             <div style={divStyle}>
                 <h1>
                     {this.state.pageTitle}
                 </h1>
-
-                {/*  <input type="text" onChange={this.inputHandler} />*/}
-                {/*<button onClick={this.changeTitleHandler.bind(this, 'Changed')}>*/}
-                {/*    Change title*/}
-                {/*</button>*/}
-
                 <button onClick={this.toggleCarsHandler}>Toggle cars</button>
 
-
-
-                {/*переходы производятся встроенными функциями js*/}
-                {this.state.showCars
-                    ?
-                    this.state.cars.map((car, index) => {
-                    return (
-                        <Car
-                            /*для оптимизации передается key*/
-                            key={index}
-                            name={car.name}
-                            year={car.year}
-                            onChangeTitle={this.changeTitleHandler.bind(this, car.name)}
-
-                          /*  1. функция не создает своего контекста(стрелочные функции).*/
-                            onChangeName={event => this.onChangeName(event.target.value, index)}
-                           /* 2. функция имеет свой контекст, мы его связали bindom с контекстом вскго класса App*/
-                            onDelete={this.deleteHandler.bind(this, index)}
-                        />
-
-                    )
-                })
-                    : null
-                }
-
-                {/*<Car*/}
-                {/*    name={cars[0].name}*/}
-                {/*    year={cars[0].year}*/}
-                {/*    //свое событие*/}
-                {/*    onChangeTitle = {this.changeTitleHandler.bind(this, cars[0].name)}*/}
-                {/*>*/}
-
-                {/*    <p style={{color: "red"}}>Color</p>*/}
-                {/*</Car>*/}
-                {/*<Car*/}
-                {/*    name={cars[1].name}*/}
-                {/*    year={cars[1].year}*/}
-                {/*    onChangeTitle = {() => this.changeTitleHandler(cars[1].name)}*/}
-                {/*/>*/}
-
-                {/*<Car*/}
-                {/*    name={cars[2].name}*/}
-                {/*    year={cars[2].year}*/}
-                {/*    onChangeTitle = {() => this.changeTitleHandler(cars[2].name)}*/}
-                {/*/>*/}
+                <div style={{
+                    width: 400,
+                    margin: 'auto',
+                    paddingTop: '20px'
+                }}>
+                    {cars}
+                </div>
             </div>
         );
     }
--- React/react-theory/src/Car/Car.js
+++ React/react-theory/src/Car/Car.js
@@ -1,49 +1,48 @@
 import React from "react";
-
-/*function car () {
-    return (
-        <div>This is car component</div>
-    );
-}*/
-/*const car = () => {
-    return (
-        <div>This is car component</div>
-    );
-}*/
-/*const car = () => (
-    <div>This is car component
-        <strong>Nissan</strong>
-    </div>
-)*/
-
-/*
-export default car;*/
-
-/*
-// return простой верстки достаточно просто круглых скобок без фигурных скобок
-export default () => (
-    <div>
-        <p>This is car component</p>
-        <strong> Nissan {Math.round(Math.random() * 100)}</strong>
-    </div>
-)*/
+import Radium from "radium";
+import './Car.scss'
 
 //для отрисовки дочерних элементов color    {props.children}
 //обращаемся к событию объявленному в App компонента props.onChangeTitle
-//<Car/>
-export default (props) => (
-    <div style={{
+
+const Car = props => {
+    const inputClasses = ['input'];
+
+    if (props.name !== '') {
+        inputClasses.push('green')
+    } else {
+        inputClasses.push('red')
+    }
+
+    if (props.name.length > 4) {
+        inputClasses.push('bold');
+    }
+
+    console.log(props)
+    const style = {
         border: '1px solid #ccc',
-        marginBottom: '10px',
-        display: 'block',
-        padding: '10px'
-    }}>
-        <h3>Car name: {props.name}</h3>
-        <p>Year: <strong> {props.year}</strong></p>
-        {/*по событию onChange вызывать onChangeName в App.js*/}
-        <input type='text' onChange={props.onChangeName} value={props.name} />
-        <button onClick={props.onChangeTitle}>Click</button>
-        <button onClick={props.onDelete}>Delete</button>
-        {props.children}
-    </div>
-)
+        boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
+        ':hover': {
+            border: '1px solid #aaa',
+            boxShadow: '0 44px 15px 0 rgba(0, 0, 0, .25)'
+        }
+    }
+    return (
+        <div className={'Car'} style={style}>
+            <h3>Car name: {props.name}</h3>
+            <p>Year: <strong> {props.year}</strong></p>
+            {/*по событию onChange вызывать onChangeName в App.js*/}
+            <input
+                type='text'
+                onChange={props.onChangeName}
+                value={props.name}
+                className={inputClasses.join(' ')}
+            />
+            <button onClick={props.onChangeTitle}>Click</button>
+            <button onClick={props.onDelete}>Delete</button>
+            {props.children}
+        </div>
+    )
+}
+
+export default Radium(Car);
