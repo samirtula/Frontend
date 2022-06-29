4eb5d0999ce326a26ce5a28b7d605a470f040606
Samir <samirtula@mail.ru>
2022-06-30 00:55:18 +0300

    Vue3 init

---
+++ Vue/v3/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button @click="addLike">Like</button>
+    <button @click="addDislike">Dislike</button>
+    <div>Количество лайков: {{ likes }}</div>
+    <div>Количество дизлайков: {{ dislikes }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      likes: 0,
+      dislikes: 2
+    }
+  },
+
+  methods: {
+    addLike() {
+      this.likes++
+    },
+    addDislike() {
+      this.dislikes++
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
