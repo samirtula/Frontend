06089543b24cfcc70674822539f5941039d543f6
Самир <s.gahramanov@intensa.ru>
2022-06-30 17:54:27 +0300

    слушатели

--- Vue/v3/src/App.vue
+++ Vue/v3/src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <div>
-    <button @click="addLike">Like</button>
-    <button @click="addDislike">Dislike</button>
-    <div>Количество лайков: {{ likes }}</div>
-    <div>Количество дизлайков: {{ dislikes }}</div>
+  <div class="app">
+    <form @submit.prevent>
+      <input class="input" type="text" @input="title = $event.target.value"  v-bind:value="title" placeholder="Название">
+      <input class="input" type="text" @input="body = $event.target.value" v-bind:value="body" placeholder="Название">
+      <button class="btn" @click="createPost">Создать</button>
+    </form>
   </div>
 </template>
 
@@ -12,22 +13,56 @@ export default {
   name: "App",
   data() {
     return {
-      likes: 0,
-      dislikes: 2
+      title: '',
+      body: ''
     }
   },
 
   methods: {
-    addLike() {
-      this.likes++
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body
+      }
+      this.posts.push(newPost);
+      this.title = '';
+      this.body = '';
     },
-    addDislike() {
-      this.dislikes++
-    }
   }
 }
 </script>
 
 <style scoped>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
+  .app {
+    padding: 20px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .btn {
+    margin-top: 15px;
+    align-self: flex-end;
+    padding: 10px 15px;
+    background: none;
+    color: teal;
+    border: 1px solid;
+  }
+
+  .input {
+    width: 100%;
+    border: 1px solid teal;
+    padding: 10px 15px;
+    margin-top: 15px;
+    outline: none;
+  }
 </style>
---
+++ Vue/v3/src/components/PostList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="post" v-for="post in posts">
+    <div><strong>Название:</strong> {{ post.title}}</div>
+    <div><strong>Описание:</strong> {{ post.body }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostList",
+  data() {
+    return {
+      posts : [
+        {id: 1, title: 'Javascript', body: 'Описание поста'},
+        {id: 2, title: 'Javascript 2', body: 'Описание поста 2'},
+        {id: 3, title: 'Javascript 3', body: 'Описание поста 3'}
+      ],
+    }
+  }
+}
+</script>
+
+<style scoped>
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+</style>
